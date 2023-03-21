c75d2315beb91565f438c449bf31ab5135c9bced
Martin Madzin <madzin.m@gmail.com>
2023-03-21 22:34:11 +0100

    cpp template added

---
+++ template.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define contains(s,x) ((s).find(x) != (s).end())
+
+#define print(x) cout << (x) << endl;
+#define dmp(x) cerr << #x << " = " << x << endl
+#define dmpl(x) cerr << "Line " << __LINE__ << ": " << #x << " = " << x <<endl
+
+// pair printing
+template <class T, class U>
+ostream& operator<<(ostream& out, const pair<T, U> &par) {out << "(" << par.first << "; " << par.second << ")"; return out;}
+// set printing
+template <class T>
+ostream& operator<<(ostream& out, const set<T> &cont) { out << "{"; for(const auto &x:cont) out << x << ", "; out << "}"; return out; }
+// map printing
+template <class T, class U>
+ostream& operator<<(ostream& out, const map<T, U> &cont) {out << "{"; for(const auto &x:cont) out << x << ", "; out << "}"; return out; }
+// unordered_set printing
+template <class T>
+ostream& operator<<(ostream& out, const unordered_set<T> &cont) {out << "{";for(const auto &x:cont) out << x << ", ";out << "}";return out;}
+// unordered_map printing
+template <class T, class U>
+ostream& operator<<(ostream& out, const unordered_map<T, U> &cont) {out << "{";for(const auto &x:cont) out << x << ", ";out << "}";return out;}
+// vector printing
+template<class T>
+ostream& operator<<(ostream& out, const vector<T> &cont){ out << "[";  for (const auto &x:cont) out << x << ", ";  out << "]"; return out;}
+
+// ordered set whith s.order_of_key(x) method, which returns rank of element x in set s
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+}
+
+int32_t main() {
+        std::cout << std::boolalpha;
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+
+        int t = 1;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+}
