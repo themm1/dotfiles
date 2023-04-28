51af67855e4d54e240a61386e5add43a153c92b6
Martin Madzin <madzin.m@gmail.com>
2023-04-28 20:14:25 +0200

    template cpp edit

--- template.cpp
+++ template.cpp
@@ -1,23 +1,12 @@
 #include <bits/stdc++.h>
+using namespace std;
+// ordered set whith s.order_of_key(x) method, which returns rank of element x in set s
+/*
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-using namespace std;
-
-using ll = long long;
-using ld = long double;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-#define pb push_back
-#define ff first
-#define ss second
-#define all(x) begin(x), end(x)
-#define sz(x) (int)x.size()
-#define contains(s,x) ((s).find(x) != (s).end())
-
-#define print(x) cout << (x) << endl;
-#define dmp(x) cerr << #x << " = " << x << endl
-#define dmpl(x) cerr << "Line " << __LINE__ << ": " << #x << " = " << x <<endl
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+*/
 
 // pair printing
 template <class T, class U>
@@ -38,14 +27,26 @@ ostream& operator<<(ostream& out, const unordered_map<T, U> &cont) {out << "{";f
 template<class T>
 ostream& operator<<(ostream& out, const vector<T> &cont){ out << "[";  for (const auto &x:cont) out << x << ", ";  out << "]"; return out;}
 
-// ordered set whith s.order_of_key(x) method, which returns rank of element x in set s
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define print(x) cout << (x) << endl;
+#define dmp(x) cerr << #x << " = " << x << endl
+#define dmpn(x) cerr << #x << " = " << x << "; "
+#define dmpl(x) cerr << "Line " << __LINE__ << ": " << #x << " = " << x << endl
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define contains(s,x) ((s).find(x) != (s).end())
 
 void solve() {
 }
 
 int32_t main() {
-        std::cout << std::boolalpha;
         ios::sync_with_stdio(false);
         cin.tie(0);
 
